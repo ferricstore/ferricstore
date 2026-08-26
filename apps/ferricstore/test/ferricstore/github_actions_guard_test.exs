2a654cbeb082baf64ff2c40cdf2a502882b610ec
Yoav Geva <dryoavgeva@gmail.com>
2026-08-26 15:05:24 +0300

    test: pin corrected Java concurrency suite

--- apps/ferricstore/test/ferricstore/github_actions_guard_test.exs
+++ apps/ferricstore/test/ferricstore/github_actions_guard_test.exs
@@ -226,7 +226,7 @@ defmodule Ferricstore.GitHubActionsGuardTest do
     assert workflow =~ "repository: ferricstore/ferricstore-elixir\n            ref: v0.11.13"
 
     assert workflow =~
-             "repository: ferricstore/ferricstore-java\n            ref: 6aa176e790d54fe2702869be2eaf3d5a0fc966cc"
+             "repository: ferricstore/ferricstore-java\n            ref: eeb9150a730133a32d886ce8c485ae31753b4f8a"
 
     refute workflow =~ "ref: v0.11.6"
 
@@ -257,7 +257,7 @@ defmodule Ferricstore.GitHubActionsGuardTest do
     test_workflow = File.read!(Path.join(@repo_root, ".github/workflows/test.yml"))
 
     assert test_workflow =~
-             "repository: ferricstore/ferricstore-java\n          ref: 6aa176e790d54fe2702869be2eaf3d5a0fc966cc"
+             "repository: ferricstore/ferricstore-java\n          ref: eeb9150a730133a32d886ce8c485ae31753b4f8a"
   end
 
   test "container workflows publish only to the official Quay.io repository" do
